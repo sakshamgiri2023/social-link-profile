193846eb23ed5e35312b7e3c7e7ad4a0d4133d67
sakshamgiri500 <sakshamgiri500@gmail.com>
2025-03-27 01:22:09 +0530

    fuck you tonny

---
+++ script.js
@@ -0,0 +1,9 @@
+const github = get.document.getElementById('Github');
+github.style.color = hsl(75, 94%, 57%);
+// const linkedin = get.document.getElementById('Linkedin')={value:changecolor('lightblue')};
+// const twitter = get.document.getElementById('Twitter')={value:changecolor('lightcoral')};
+// const facebook = get.document.getElementById('Facebook')={value:changecolor('lightyellow')};    
+// const linkedin = get.document.getElementById('Linkedin').value =changecolor('lightblue');
+// const twitter = get.document.getElementById('Twitter').value =changecolor('lightcoral');
+// const facebook = get.document.getElementById('Facebook').value =changecolor('lightyellow');
+// const instagram = get.document.getElementById('Instagram').value =changecolor('lightpink');
